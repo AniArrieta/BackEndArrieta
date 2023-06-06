ab20de4dff7da903bf0a2cbfa6abf0381db1b008
AniArrieta <arrietar.ana@gmail.com>
2023-06-06 17:40:39 +0200

    comento crossorigin

--- src/main/java/com/portfolioarrieta/backendarrieta/Controller/PersonaController.java
+++ src/main/java/com/portfolioarrieta/backendarrieta/Controller/PersonaController.java
@@ -4,7 +4,7 @@ import com.portfolioarrieta.backendarrieta.Entity.Persona;
 import com.portfolioarrieta.backendarrieta.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://portfolioarrieta.web.app")
+//@CrossOrigin(origins = "https://portfolioarrieta.web.app")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
